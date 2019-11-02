3ea61bb004e4a08c8426a0ddea60b1a7b8d09874
JeremiahSifuentes <jerrysifuentes01@gmail.com>
2019-11-02 18:54:07 -0500

    Services css added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
  
 app.get('/', function (req, res) {
-  res.render("home.ejs")
-})
+  res.render("Services.ejs");
+});
  
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
